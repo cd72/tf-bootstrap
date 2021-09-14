780bf84a23904485d291ec3c7bbdac4f4eddac50
cd72 <chris.j.denney@gmail.com>
2021-09-14 22:24:39 +0100

    bug fix

--- local_output.tf
+++ local_output.tf
@@ -1,4 +1,4 @@
-resource "local_file" "foo" {
-    content     = "aws s3 cp terraform.tfstate s3://${data.aws_caller_identity.current.account_id}}-terraform-state/tf-bootstrap/terraform.tfstate"
-    filename = "${path.module}/tf.s3command.out"
+resource "local_file" "script_s3" {
+    content     = "aws s3 cp terraform.tfstate s3://${data.aws_caller_identity.current.account_id}-terraform-state/tf-bootstrap/terraform.tfstate"
+    filename = "${path.module}/tf.s3command.sh"
 }
